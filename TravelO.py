34bb0e36faca0185ed58a632524c9820246fe18f
Patrick Sullivan <sublime@vt.edu>
2018-11-18 19:01:01 -0500

    starting code outline

---
+++ TravelO.py
@@ -0,0 +1,40 @@
+# import beautiful soup?
+from time import time
+from date import date
+# import read file? 
+
+# TODO: install submlime plugin for easy edit CSVs
+# TODO: install submlime plugin for easy open explorer
+
+# constants OR read config file
+MPG = 27
+pricePerGal = 2.29
+hourWage = (24000 / 52 ) / 40 
+airportTimeAdd = time(1, 30, 0)
+driveTimeAdd  = time(0, 0, 0)
+shuttleTimeAdd = time(0,0,0)
+airportTimeMulti = 1.00
+driveTimeMulti  = 1.00
+shuttleTimeMulti = 1.00
+
+
+scheduleStrictness = 10
+searchDayStart = date("December 18 2018")
+searchDayEnd = date("Janurary 8 2019")
+
+
+def main():
+
+	# read driveDest.csv validate?
+	# make requests to google flights
+		# start with longest trip length possible
+		# dynamic stop?
+
+	# match driveDest to google flights numbers
+
+
+
+
+
+if __name__ == '__main__':
+	main()
