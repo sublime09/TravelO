f647ee55204ce51b050a69816a9d477e9bcb6bb2
Patrick Sullivan <sublime@vt.edu>
2018-11-18 19:59:02 -0500

    pep8 clean

--- TravelO.py
+++ TravelO.py
@@ -4,11 +4,10 @@ from time import time
 from datetime import time, date
 
 import gtfo
-url = gtfo.roundtrip().departing("JFK").returning("JNB").open()
-print(url)
+url = gtfo.roundtrip().departing("JFK").returning("JNB").url()
 exit()
 
-# import read file? 
+# import read file?
 
 # TODO: install submlime plugin for easy edit CSVs
 # TODO: install submlime plugin for easy open explorer
@@ -17,12 +16,12 @@ exit()
 # constants OR read config file
 MPG = 27
 pricePerGal = 2.29
-hourWage = (24000 / 52 ) / 40 
+hourWage = (24000 / 52) / 40
 airportTimeAdd = time(1, 30, 0)
-driveTimeAdd  = time(0, 0, 0)
-shuttleTimeAdd = time(0,0,0)
+driveTimeAdd = time(0, 0, 0)
+shuttleTimeAdd = time(0, 0, 0)
 airportTimeMulti = 1.00
-driveTimeMulti  = 1.00
+driveTimeMulti = 1.00
 shuttleTimeMulti = 1.00
 
 searchDayStart = date(2018, 12, 18)
@@ -30,15 +29,16 @@ searchDayEnd = date(2019, 1, 8)
 searchMinTripDays = 10
 assert (searchDayEnd - searchDayStart).days > searchMinTripDays
 
-def main():
-	pass
-	# read driveDest.csv validate?
-	# make requests to google flights
-		# start with longest trip length possible
-		# dynamic stop?  nonono too many requests can be banned
 
-	# match driveDest to google flights numbers
+def main():
+    pass
+    # read driveDest.csv validate?
+    # make requests to google flights
+    # start with longest trip length possible
+    # dynamic stop?  nonono too many requests can be banned
+
+    # match driveDest to google flights numbers
 
 
 if __name__ == '__main__':
-	main()
+    main()
